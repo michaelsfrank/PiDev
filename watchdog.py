768f6890996e1b18fb307060c89a3b6b3eacdec3
MichaelSFrank <Michael.S.Frank@gmail.com>
2015-10-06 22:08:52 -0400

    real watchdog.py (Pi2)

--- watchdog.py
+++ watchdog.py
@@ -1,59 +1,82 @@
 #!/usr/bin/env python
-import subprocess
+
+# http://docs.python-guide.org/en/latest/scenarios/scrape/
+
+# Requests: HTTP for Humans    http://docs.python-requests.org/en/v1.0.1/
+# sudo pip install requests
+
+# http://lxml.de/tutorial.html
+
+# https://docs.python.org/2/library/xml.etree.elementtree.html
+
+# sudo apt-get install python-lxml
+# http://lxml.de/parsing.html
+
+from lxml import html
+import requests
 import datetime
 import time
-import sqlite3
-#import sys
+import subprocess
 
-#from datetime import date
-#import matplotlib.dates as dt
-import apsw
+def dtt2timestamp(dtt):
+    ts = (dtt.hour * 60 + dtt.minute) * 60 + dtt.second
+    #if you want microseconds as well
+    #ts += dtt.microsecond * 10**(-6)
+    total=24*60*60
+    result=float(ts)/float(total)
+    #print ts, " / ", total, " = ", result
+    return result
 
-dbname='/var/www/Logger.db'
 
-table_time_delta_limit=0.010416667
-# 60min x 24 hrs = 1440 min/day
-# 1440 min/day ^-1 = 0.00069444 day/min
-# 0.00069444 day/min x 15min = 0.010416667
+page = requests.get('http://frafle.ddns.net/cgi-bin/table-age-cpu2.py?Table=Systems_Pi2_Temp_CPUTemp')
 
-# http://www.ridgesolutions.ie/index.php/2013/02/22/raspberry-pi-restart-shutdown-your-pi-from-python-code/
+# http://lxml.de/lxmlhtml.html
+root = html.fromstring(page.text)
 
-#conn=sqlite3.connect(dbname)
-conn=apsw.Connection(dbname,flags=1)
-curs=conn.cursor()
+#print "root: ", root
+#print "root.tag: ", root.tag
+#print "root.attrib: ", root.attrib
 
-table_time_max = [str(x[0]) for x in buf]
+#for child in root:
+#    print "  child: ", child
+#    print "  child.tag: ", child.tag
+#    print "  child.attrib: ", child.attrib
+#    print "  child.text: ", child.text
+#    #print "  child.value: ", child.value
+#    for subchild in child:
+#        print "    subchild: ", subchild
 
-for x in range(0, 1000):
-    try:
-        curs.execute("SELECT Max(Time) FROM Systems_Pi3_Temp_CPUTemp")
-        buf=curs.fetchall()
-        break;
-    except:
-        time.sleep(1)
-        buf=[(100,)]
-try:
-    print "<tr><td>", d, "</td><td>", dt.num2date(buf[0][0]), "</td></tr>"
-except TypeError:
-    print "<tr><td>", d, "</td><td> none </td></tr>"
+rightnow = float( datetime.datetime.now().toordinal() + dtt2timestamp(datetime.datetime.now().time()) )
+print "rightnow = ", rightnow
 
-conn.close()
+for element in root.iter("systems_pi2_temp_cputemp"):
+    print("%s - %s" % (element.tag, element.text))
+    print float(datetime.datetime.now().toordinal()+dtt2timestamp(datetime.datetime.now().time()))
+    #print datetime.datetime.now().time()
+    #print dtt2timestamp(datetime.datetime.now().time())
+    print float(element.text)
+    print "rightnow - max = ", rightnow, " - ", float(element.text), " = ", rightnow-float(element.text)
+    if (rightnow-float(element.text))>0.01:
+        print "reboot"
 
-ts = time.time()  
-st = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')  
-now = datetime.datetime.now().toordinal()-Period
+#        ts = time.time()  
+#        st = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')  
+#        now = datetime.datetime.now().toordinal()-Period
  
  
-f = open('/home/pi/watchdog.log', 'a')  
-# write the timestamp and text to the file  
-f.write(st)  
-f.write(' : REBOOT command issued\n') 
-f.close()
+#        f = open('/home/pi/watchdog.log', 'a')  
+        # write the timestamp and text to the file  
+#        f.write(st)  
+#        f.write(' : REBOOT command issued\n') 
+#        f.close()
 
-if datetime.datetime.now().toordinal()-table_time_max > table_time_delta_limit
-    command = "/usr/bin/sudo /sbin/shutdown -r now"
-    sys.stderr.write('sql statements set\n')
-    #process = subprocess.Popen(command.split(), stdout=subprocess.PIPE)
-    #output = process.communicate()[0]
-    #print output
- 
+        command = "/usr/bin/sudo /sbin/shutdown -r now"
+#        sys.stderr.write('sql statements set\n')
+        process = subprocess.Popen(command.split(), stdout=subprocess.PIPE)
+        output = process.communicate()[0]
+        print output
+
+    else:
+        print "OK"
+
+
